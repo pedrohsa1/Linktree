fd56b48503c8babe69e10e7ae51b74ed59e3848c
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2024-08-08 15:34:25 -0300

    Configurando tailwindcss e ajustando layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,25 @@
 
 export function Home(){
   return(
-    <div>
-      <h1>Página Home</h1>
+    <div className="flex flex-col w-full py-4 items-center justify-center">
+      <h1 className="md:text-4xl  text-3xl font-bold text-white mt-20">Pedro Dev</h1>
+      <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
+
+      <main className="flex flex-col w-11/12 max-w-xl text-center">
+        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
+          <a>
+            <p className="text-base md:text-lg">
+              Canal no Youtube
+            </p>
+          </a>
+        </section>
+
+        <footer className="flex justify-center gap-3 my-4">
+          
+        </footer>
+
+      </main>
+
     </div>
   )
 }
