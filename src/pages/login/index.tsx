db8909736d4f92ae3c4838d0756df5965d4e4aa1
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2024-08-08 15:40:27 -0300

    Criando componente login input

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,22 @@
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import { Input } from '../../components/Input'
 
 export function Login(){
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  function handleSubmit(e: FormEvent){
+    e.preventDefault();
+
+    console.log({
+      email: email,
+      password: password
+    })
+
+  }
+
   return(
     <div className="flex w-full h-screen items-center justify-center flex-col">
       <Link to="/">
@@ -8,6 +24,28 @@ export function Login(){
         <span className="bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent">Link</span>
         </h1>
       </Link>
+
+      <form onSubmit={handleSubmit}  className="w-full max-w-xl flex flex-col px-2">
+        <Input
+          placeholder="Digite o seu email..."
+          type="email"
+          value={email}
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+
+        <Input
+          placeholder="*********"
+          type="password"
+          value={password}
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+
+        <button 
+        type="submit"
+        className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white">
+          Acessar
+        </button>
+      </form>
     </div>
   )
 }
